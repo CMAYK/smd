9dccc5c6469c25a72ec09045301b333ad7f4a17f
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-02-29 20:52:14 -0600

    added camera2D script | added mario sprites

---
+++ Super Mario Defense Godot/Stages/Camera2D.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var FOLLOW_POINT = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_released("scroll_up"):
+		FOLLOW_POINT += 100
+	if Input.is_action_just_released("scroll_down"):
+		FOLLOW_POINT -= 100
+	self.position.x = lerp(self.position.x, FOLLOW_POINT, 0.05)
