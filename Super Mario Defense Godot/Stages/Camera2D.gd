3de70d8feace6b3c7b6200a66d4efed760c83254
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-03-01 23:42:03 -0600

    added maro generaton

--- Super Mario Defense Godot/Stages/Camera2D.gd
+++ Super Mario Defense Godot/Stages/Camera2D.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-var FOLLOW_POINT = 0.0
+var FOLLOW_POINT = clamp(0, 0.0, 10.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +9,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	FOLLOW_POINT = clamp(FOLLOW_POINT, -512.0, 512.0)
 	if Input.is_action_just_released("scroll_up"):
-		FOLLOW_POINT -= 50
+		FOLLOW_POINT -= 128
 	if Input.is_action_just_released("scroll_down"):
-		FOLLOW_POINT += 50
+		FOLLOW_POINT += 128
 
-	self.position.x = lerp(self.position.x, FOLLOW_POINT, 0.02)
+	self.position.x = FOLLOW_POINT
--- Super Mario Defense Godot/Stages/Maringo_Generator.gd
+++ Super Mario Defense Godot/Stages/Maringo_Generator.gd
@@ -4,14 +4,18 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-var mario = preload("res://Objects/test_mario.tscn")
+var mario_path = preload("res://Objects/test_mario.tscn")
+var spawn_time = 10
+var spawn_timer = spawn_time
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for x in 1:
+	spawn_timer -= 1
+	if spawn_timer <= 0:
+		spawn_timer = spawn_time
 		create_mario()
 
 func create_mario():
-	pass
-	#var mario = mario.instantiate()
-	#get_parent().add_child(mario)
+	var mario = mario_path.instantiate()
+	get_parent().add_child(mario)
+	mario.position = self.position
