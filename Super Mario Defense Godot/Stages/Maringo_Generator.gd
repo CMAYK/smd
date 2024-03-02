a7f95d6c9bd1b3d02a949c8daddd80560e6009ad
Snak <dtmbutu@gmail.com>
2024-03-02 03:23:04 -0500

    cool thing i did uwu

--- Super Mario Defense Godot/Stages/Maringo_Generator.gd
+++ Super Mario Defense Godot/Stages/Maringo_Generator.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 var mario_path = preload("res://Objects/test_mario.tscn")
-var spawn_time = 10
+var spawn_time = 200
 var spawn_timer = spawn_time
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	create_mario()
+
 func _process(delta):
 	spawn_timer -= 1
 	if spawn_timer <= 0:
--- Super Mario Defense Godot/Stages/camera_main.gd
+++ Super Mario Defense Godot/Stages/camera_main.gd
@@ -1,22 +1,27 @@
 extends Camera2D
 
-var FOLLOW_POINT = clamp(0, 0.0, 10.0)
-@onready var tilemap = Globals.tilemap
+@export var tilemap: TileMap
+
 
 func _ready():
-	await get_tree().create_timer(1.0).timeout
+
 	var mapRect = tilemap.get_used_rect()
 	var tileSize = tilemap.rendering_quadrant_size
 	var worldSizeInPixels = mapRect.size * tileSize
 	limit_right = worldSizeInPixels.x
 
+	print_debug(worldSizeInPixels.x)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#FOLLOW_POINT = clamp(FOLLOW_POINT, -512.0, 512.0)
 	if Input.is_action_just_released("scroll_up"):
-		FOLLOW_POINT -= 128
+		self.position.x -= 128/2
 	if Input.is_action_just_released("scroll_down"):
-		FOLLOW_POINT += 128
+		self.position.x += 128/2
 
-	self.position.x = FOLLOW_POINT
+
+	self.position.x = clamp(self.position.x, get_viewport_rect().size.x /4, limit_right - get_viewport_rect().size.x /4)
+
+	
+	print(get_viewport_rect().size.x /4)
+	
