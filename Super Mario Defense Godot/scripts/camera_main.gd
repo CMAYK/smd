456c9e2a1cf4a44167eb9866e57787fe610add27
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-03-02 22:31:41 -0600

    res change via 1,2,3

--- Super Mario Defense Godot/scripts/camera_main.gd
+++ Super Mario Defense Godot/scripts/camera_main.gd
@@ -13,11 +13,16 @@ func _limit_camera(tilemap):
 		limit_right = worldSizeInPixels.x
 
 func _physics_process(delta):
-	if Input.is_action_pressed("w"):
-		pass
+	if Input.is_action_pressed("test1"):
+		DisplayServer.window_set_size(Vector2(640, 380))
+	if Input.is_action_pressed("test2"):
+		DisplayServer.window_set_size(Vector2(640*2, 380*2))
+	if Input.is_action_pressed("test3"):
+		DisplayServer.window_set_size(Vector2(640*3, 380*3))
+
 	if Input.is_action_just_released("scroll_up"):
 		self.position.x -= 128/2
 	if Input.is_action_just_released("scroll_down"):
 		self.position.x += 128/2
 
-	self.position.x = clamp(self.position.x, get_viewport_rect().size.x /4, limit_right - get_viewport_rect().size.x /4)
+	self.position.x = clamp(self.position.x, get_viewport_rect().size.x /2, limit_right - get_viewport_rect().size.x /2)
