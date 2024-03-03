6a3599f12a5ed54e2d7a38fb8a8be94e1cacd2cb
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-03-02 22:41:48 -0600

    pissed out some cheap code

--- Super Mario Defense Godot/scripts/camera_main.gd
+++ Super Mario Defense Godot/scripts/camera_main.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 #var tilemap: TileMap
 var continue_load: bool = false
+var scroll_method = 0 #0 = scrolling / 1 = A+D / 2 = cursor position
 
 func _ready():
 	pass
@@ -13,16 +14,37 @@ func _limit_camera(tilemap):
 		limit_right = worldSizeInPixels.x
 
 func _physics_process(delta):
+	#this changes the game window size; test1, 2 and 3 correspond to placeholder inputs mapped in project settings
 	if Input.is_action_pressed("test1"):
-		DisplayServer.window_set_size(Vector2(640, 380))
+		print("base res")
+		DisplayServer.window_set_size(Vector2(640, 380)) # 1x base res
 	if Input.is_action_pressed("test2"):
-		DisplayServer.window_set_size(Vector2(640*2, 380*2))
+		print("720p")
+		DisplayServer.window_set_size(Vector2(640*2, 380*2)) # 720p
 	if Input.is_action_pressed("test3"):
-		DisplayServer.window_set_size(Vector2(640*3, 380*3))
+		print("1080p")
+		DisplayServer.window_set_size(Vector2(640*3, 380*3)) # 1080p
+	
+	#this changes scrolling input method
+	if Input.is_action_just_released("test4"):
+		scroll_method += 1
+		if scroll_method >= 4: 
+			scroll_method = 0
 
-	if Input.is_action_just_released("scroll_up"):
-		self.position.x -= 128/2
-	if Input.is_action_just_released("scroll_down"):
-		self.position.x += 128/2
+	match scroll_method:
+		0: #scroll wheel input
+			print("scrolling with mw")
+			if Input.is_action_just_released("scrollw_up"):
+				self.position.x -= 128/2
+			if Input.is_action_just_released("scrollw_down"):
+				self.position.x += 128/2	
+		1: #scroll via A and D input
+			print("scrolling with A/D")
+			if Input.is_action_just_pressed("scrollb_up"):
+				self.position.x -= 128/2
+			if Input.is_action_just_pressed("scrollb_down"):
+				self.position.x += 128/2	
+		2: #a third secret scrolling option
+			pass
 
 	self.position.x = clamp(self.position.x, get_viewport_rect().size.x /2, limit_right - get_viewport_rect().size.x /2)
