b3541a7c2428d515bf3643c1de80c3b859f0d9fa
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-03-13 23:20:37 -0500

    castle works wip

--- Super Mario Defense Godot/scenes/playstate.gd
+++ Super Mario Defense Godot/scenes/playstate.gd
@@ -8,10 +8,12 @@ var tilemap
 
 var sound_player = preload("res://game/sounds/sound_player.tscn")
 var hero_spawner = preload("res://game/heroes/hero_spawner/hero_spawner.tscn")
+var castle = preload("res://game/layouts/castle.tscn")
 
 func _ready():
 	_load_layout()
 	_load_spawner()
+	_load_castle()
 	camera._limit_camera(tilemap)
 
 func _load_layout():
@@ -25,3 +27,8 @@ func _load_spawner():
 	var hs_instance = hero_spawner.instantiate()
 	hs_instance.position = layout.spawn_pos.position
 	$heros.add_child(hs_instance)
+	
+func _load_castle():
+	var c_instance = castle.instantiate()
+	c_instance.position = layout.castle_pos.position
+	$heros.add_child(c_instance)
