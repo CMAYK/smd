9f5eeda033897686938acb8853804eb1449bdc8d
CMAYK <44140089+CMAYK@users.noreply.github.com>
2024-03-15 22:46:13 -0500

    thats all for tonight

--- Super Mario Defense Godot/game/scripts/globals.gd
+++ Super Mario Defense Godot/game/scripts/globals.gd
@@ -1,18 +1,23 @@
 extends Node
+#human, I remember your globals
 
 var tilemap: TileMap
 
-var time = 0.1
-var timer = 0.1
-var colormod = 0
+#timer for color modulataion on bowser statue laser
+var time = 0.1 #probably could've used a better var name for this
+var timer = 0.1 #probably could've used a better var name for this
+var colormod = 0 #probably could've used a better var name for this
+
+var basehealth = 100
 
 func _ready():
 	timer = time
+	basehealth = 100
 
 func _process(delta):
-	timer -= 1
+	timer -= 1 
 	if timer <= 0:
 		timer = time
 		colormod += 1
-		if colormod >= 5:
+		if colormod >= 5: #5 is the amount of colors used in the modulation array in the bowser statue laser
 			colormod = 0
